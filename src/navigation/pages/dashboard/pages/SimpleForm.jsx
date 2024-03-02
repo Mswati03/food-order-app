5d265047a5c1fab55076ea695571b157f5a886da
Mswati03 <mswatitshabalala03@gmail.com>
2024-03-01 17:21:20 -0800

    review bot updates

--- src/navigation/pages/dashboard/pages/SimpleForm.jsx
+++ src/navigation/pages/dashboard/pages/SimpleForm.jsx
@@ -1,12 +1,19 @@
-import React, { Component } from 'react'
+import React, { Component, useState } from 'react'
 import ChatBot from 'react-simple-chatbot';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
+import { getAuth } from "firebase/auth";
 
 
 const  SimpleForm = () => {
+    const [response, setResponse] = useState('');
+    const db = firebase.firestore();
+    const auth = getAuth();
+    const user = auth.currentUser;
     const steps = [
         {
             id: '1',
-            message: 'What is your name?',
+            message: 'Leave a review',
             trigger: '3'
         },
         {
@@ -19,18 +26,48 @@ const  SimpleForm = () => {
             id: '3',
            user: true,
            trigger: '2',
+           validator: (value) => {
+            if (value=='') {
+              return 'value should be a number';
+            }
+            setResponse(value);
+           
+            return true;
+          },
             
         }
        
     ];
-console.log(steps);
+    const [review, setReview] = useState('');
+
+    const handleUserInput = (e) => {
+        console.log('User input received:', value); // Log user input
+        setReview(e.target.value);
+    }
+    const uid = user.uid;
+    console.log(response);  // Log current review state
+    db.collection("reviews").doc(uid).set({
+        message : response
+       }).then(( docRef)=>{
+        const docId= docRef.id;
+        console.log(docId);
+    
+       }).catch((error) => {
+        console.error("Error adding order to Firestore: ", error);
+      });
+    
     return (
         <div>
             <ChatBot
+                headerTitle="Instant Eats Reviews"
                 steps={steps}
-                floating={true}
-                optionStyle={{ fontSize: '20px', padding: '10px', margin: '5px' }} />
+                floating = {true}
+                userInput={handleUserInput} // Pass the function to handle user input
+                handleEnd={() => console.log('Chatbot ended')}
+           
+           /> 
+           
         </div>
-    )
+    );
 }
 export default SimpleForm;
